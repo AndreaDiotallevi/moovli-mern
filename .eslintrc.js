5470bbb3f9c31d0d1f73030d41749229f015b7bf
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-04 16:31:22 +0000

    Chore: eslint added with Airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,7 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    "extends": [ "airbnb"
     ],
     "rules": {
+      "no-console":"off"
     }
 };
