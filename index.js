0bdec23f72877d82027a166d339488a2383da1cd
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-05 15:00:19 +0000

    fix: refactor express sample test

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('/api/getList', (req,res) => {
     var list = ["item1", "item2", "item3"];
     res.json(list);
-    console.log(res.json(list))
     console.log('Sent list of items');
 });
 
