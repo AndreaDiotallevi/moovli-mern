75e10bb0cd935697d823538ea22a88958b7ebe30
Daniel <demartinezgatell@gmail.com>
2020-02-05 17:28:37 +0000

    feat: user routes and validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://moovli:moovli@moovli-qjsyq.mongodb.net/test?retryWrites=true&w=majority" 
+  mongoURI: "mongodb+srv://moovli:moovli@moovli-qjsyq.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
